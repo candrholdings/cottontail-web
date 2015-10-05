c29c2576e28f4db855440b39736dfd3df1919321
compulim <compulim@hotmail.com>
2015-10-06 04:35:39 +0800

    Disable assertEquals

--- js/webdriver/assertions/equals.js
+++ js/webdriver/assertions/equals.js
@@ -1,22 +1,22 @@
-!function () {
-    'use strict';
+// !function () {
+//     'use strict';
 
-    window.App.WebDriver.Commands.assertEqual = {
-        parameters: ['expected', 'message'],
-        platforms: {
-            webdriverio: function (parameters) {
-                var code = ['.assertEqual(', JSON.stringify(parameters.expected)],
-                    {message} = parameters;
+//     window.App.WebDriver.Commands.assertEqual = {
+//         parameters: ['expected', 'message'],
+//         platforms: {
+//             webdriverio: function (parameters) {
+//                 var code = ['.assertEqual(', JSON.stringify(parameters.expected)],
+//                     {message} = parameters;
 
-                message && code.push(', ', JSON.stringify(message));
+//                 message && code.push(', ', JSON.stringify(message));
 
-                return code.join('') + ')';
-            },
-            spec: function (parameters) {
-                var {message} = parameters;
+//                 return code.join('') + ')';
+//             },
+//             spec: function (parameters) {
+//                 var {message} = parameters;
 
-                return message ? message : 'Expect ' + JSON.stringify(parameters.expected);
-            }
-        }
-    };
-}();
+//                 return message ? message : 'Expect ' + JSON.stringify(parameters.expected);
+//             }
+//         }
+//     };
+// }();
