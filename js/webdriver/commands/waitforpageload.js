2e45c4d4f22cfdd08ce7e71b175efdd68fa86960
compulim <compulim@hotmail.com>
2015-10-06 23:14:57 +0800

    Add waitForPageLoad command

---
+++ js/webdriver/commands/waitforpageload.js
@@ -0,0 +1,13 @@
+!function () {
+    'use strict';
+
+    window.App.WebDriver.Commands.waitForPageLoad = {
+        parameters: ['ms'],
+        platforms: {
+            webdriverio: function (parameters) {
+                return '.waitForPageLoad(' + JSON.stringify(parameters.ms || 500) + ')';
+            },
+            spec: 'Wait until page load completed within :ms ms'
+        }
+    };
+}();
