908b86314a4b4745728ac03021af62e1fe95f869
compulim <compulim@hotmail.com>
2015-10-06 23:14:46 +0800

    Typo

--- js/webdriver/commands/waitforexist.js
+++ js/webdriver/commands/waitforexist.js
@@ -5,7 +5,7 @@
         parameters: ['selector', 'ms', 'reverse'],
         platforms: {
             webdriverio: function (parameters) {
-                return '.click(' + JSON.stringify(parameters.selector) + ', ' + JSON.stringify(parameters.ms || 500) + ', ' + JSON.stringify(parameters.reverse || false) + ')';
+                return '.waitForExist(' + JSON.stringify(parameters.selector) + ', ' + JSON.stringify(parameters.ms || 500) + ', ' + JSON.stringify(parameters.reverse || false) + ')';
             },
             spec: 'Wait until element ":selector" exists'
         }
